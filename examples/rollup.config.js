0651f419bcd9d21747e3b380f89affc44a2a2c67
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-12-18 14:23:12 -0500

    examples: declare missing wasi externals

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,11 +1,13 @@
-// Only used for examples
-
 import typescript from '@rollup/plugin-typescript';
 
 export default function(args) {
     return {
         input: `./examples/${args.configExample}.ts`,
-        external: ['wasi:webgpu/webgpu'],
+        external: [
+            'wasi:webgpu/webgpu',
+            'wasi:webgpu/surface',
+            'wasi:webgpu/graphics-context',
+        ],
         output: {
             file: `temp/${args.configExample}.js`,
             format: 'esm'
